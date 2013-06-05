aa42ea1b138dde6e8fc22d9f67e3877149425a85
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2013-06-05 14:55:42 +0200

    Added KIDSWCS, KIDS_photocal and KIDS_epoch, corresponding to there PS1 counterparts.

--- lenstractor/kids.py
+++ lenstractor/kids.py
@@ -26,10 +26,30 @@ vb = True
 
 # ============================================================================
 
+def KIDSWCS(hdr):
+      '''
+      Return a WCS object initialised from a KIDS file header.
+      WARNING: PC matrix not being used, determinant may be wrong sign...
+      Need to check with literature image of H1413
+
+      TODO: CD1_1 has a negative value in KIDS where CDELT1 in PS1 is
+            positive. Need to check whether this is correct.
+      '''
+      t = util.Tan()
+      t.set_crpix(hdr['CRPIX1'], hdr['CRPIX2'])
+      t.set_crval(hdr['CRVAL1'], hdr['CRVAL2'])
+      t.set_cd(hdr['CD1_1'], 0., 0., hdr['CD2_2'])
+      t.set_imagesize(hdr['NAXIS1'], hdr['NAXIS2'])
+      
+      return tractor.FitsWcs(t)
+
+
 # ============================================================================
-# Return the image quality (FWHM) in pixels given a FITS file header.
 
 def KIDS_IQ(hdr):
+   """
+   Return the image quality (FWHM) in pixels given a FITS file header.
+   """
    # TODO: Determine the correct units of PSF_RAD.
    FWHM = hdr['PSF_RAD'] # arcsec
    # Need it in pixels:
@@ -37,3 +57,29 @@ def KIDS_IQ(hdr):
    FWHM /= 0.2
    
    return FWHM
+
+# ============================================================================
+
+def KIDS_photocal(hdr):
+   """
+   Return a PS1MagsPhotoCal object given a FITS file header.
+   """
+   # TODO: The KiDS zeropoint is always 0 in CoaddedRegriddedFrames.
+   #       This might not give the desired result.
+   band = hdr['FILT_ID'][5] # e.g. 'OCAM_g_SDSS'[5] -> 'g'
+   zpt = hdr['ZEROPNT']
+   photocal = lenstractor.PS1MagsPhotoCal(zpt,band)
+
+   return band,photocal
+
+
+# ============================================================================
+
+def KIDS_epoch(hdr):
+   """
+   Return the MJD given a FITS file header.
+   """
+   # TODO: OBS_STRT has a value like '2013-04-07T06:21:19' in KiDS, while
+   #       the 'MJD-OBS' in PS1 is a number converted to a string.
+   return hdr['OBS_STRT']
+
