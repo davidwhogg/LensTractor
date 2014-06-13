Switch to NanoMaggies brightness units.  Re: sky: no need to set stepsize; it is linear so produces the correct derivative regardless

lenstractor/dm.py
@@ -165,9 +165,6 @@ def Deal(scifiles,varfiles,SURVEY='PS1',vb=False):
       # Set up sky to be varied:
       median = np.median(sci[invvar > 0])
       sky = tractor.ConstantSky(median)
-      delta = 0.1*np.sqrt(1.0/np.sum(invvar))
-      assert delta > 0
-      sky.stepsize = delta
       if vb: print sky
 
       # Get WCS from FITS header:
@@ -327,7 +324,7 @@ def Initial_PSF(FWHM,double=False):
 # ============================================================================
 # Compute suitable mean centroid and magnitudes in each band:
 
-def Turnover(allbands,allmagnitudes,allcentroids,vb=False):
+def Turnover(allbands,allfluxes,allcentroids,vb=False):
 
     # Models need good initial fluxes to avoid wasting time getting these 
     # right. Take a quick look at the data to do this:
@@ -349,8 +346,10 @@ def Turnover(allbands,allmagnitudes,allcentroids,vb=False):
     magnitudes = np.zeros(len(bandnames))
     for i,bandname in enumerate(bandnames):
         index = np.where(allbands == bandname)
-        magnitudes[i] = np.median(allmagnitudes[index])
-    SED = tractor.Mags(order=bandnames, **dict(zip(bandnames,magnitudes)))
+        magnitudes[i] = np.median([allfluxes[i].getFlux(bandname)
+                                   for i in index])
+    SED = tractor.NanoMaggies(order=bandnames,
+                              **dict(zip(bandnames,magnitudes)))
     if vb: print "Mean SED: ",SED
 
     return centroid,SED

lenstractor/model.py
@@ -128,7 +128,11 @@ class Model():
             galpos = position
         # Give it its fair share of the flux, and start faint:
         fudge = 0.2
-        galSED = SED.copy() + 2.5*np.log10((self.K+1)/fudge)
+
+        # ASSUME linear flux units (NanoMaggies)
+        galSED = SED.copy()
+        galSED.setAllParams(np.array(galSED.getAllParams()) * fudge / (self.K+1))
+
         # Some standard shape and size parameters:
         re = resetre    # in arcsec, probably appropriate for the SQLS examples?
         q = 0.8     # axis ratio
@@ -148,7 +152,10 @@ class Model():
                 e = 0.01 # arcsec, MAGIC
                 dx,dy = e*np.random.randn(2)/3600.0
                 starpos = position.copy() + tractor.RaDecPos(dx,dy)
-            starSED = SED.copy() + 2.5*np.log10((self.K+1)/fudge)
+            # ASSUME linear brightness units
+            starSED = SED.copy()
+            starSED.setAllParams(np.array(starSED.getAllParams()) * fudge / (self.K+1))
+
             # Package up:
             star = tractor.PointSource(starpos,starSED)
             if self.vb: print star

lenstractor/sdss.py
@@ -215,7 +215,7 @@ def getSDSSdata(rcf,roi,datadir,vb=False):
     bands = []
     images = []
     centroids = []
-    total_mags = []
+    total_fluxes = []
     
     if vb:
         print "Querying SDSS skyserver for data at ra,dec = ",ra,dec
@@ -233,8 +233,9 @@ def getSDSSdata(rcf,roi,datadir,vb=False):
             print "--------"+band+"-band--------"
         
         # Get SDSS image:
-        image,info = st.get_tractor_image_dr9(run, camcol, field, band, roiradecsize=geometry)
-
+        image,info = st.get_tractor_image_dr9(run, camcol, field, band,
+                                              roiradecsize=geometry,
+                                              nanomaggies=True)
         bands.append(band)
         images.append(image)
 
@@ -242,19 +243,20 @@ def getSDSSdata(rcf,roi,datadir,vb=False):
         centroid = tractor.RaDecPos(ra,dec)
         centroids.append(centroid)
 
-        # Compute total magnitude for this image:
-        background = np.median(image.data)
-        diffimage = image.data - background
-        total_flux = np.sum(diffimage)
-        total_mag = image.photocal.countsToMag(total_flux)
-        total_mags.append(total_mag)
+        # Compute total flux for this image.
+        # ASSUME:
+        # SDSS DR8+ images are sky-subtracted and in units of nanomaggies.
+        # Therefore no background subtraction or scaling from "counts" to
+        # Nanomaggies units.
+        total_flux = image.getImage().sum()
+        total_fluxes.append(tractor.NanoMaggies(**{band: total_flux}))
         
         if vb: 
             print "Got pixel data in the "+band+" band"
             print "Image size: ",image.data.shape,"pixels"
-            print "Total brighness of image (mags): ",total_mag
+            print "Total flux of image: ",total_flux
 
 
-    return images,centroids,np.array(total_mags),np.array(bands)
+    return images,centroids,total_fluxes,np.array(bands)
 
 # ============================================================================